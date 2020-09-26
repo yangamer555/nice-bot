fc901dd6867d18cbfcac30c59d081327dc5bbb59
yangamer555 <60856839+yangamer555@users.noreply.github.com>
2020-09-26 17:54:56 +0300

    Update vote.js

--- commands/vote.js
+++ commands/vote.js
@@ -11,33 +11,11 @@ exports.run = async (bot, message, args) => {
   // Number.isInteger(itime)
   //  if (e) return message.reply('please supply a valid time number in seconds')
   
-  let msg = await message.channel.send(`Question: ${message.content.split(" ").splice(2).join(" ")} \nГолосуйте сейчас! (Время голосования: ${args[0]}ms)`);
+  let msg = await message.channel.send(`Опрос: ${message.content.split(" ").splice(2).join(" ")} \nГолосуйте сейчас!`);
   await msg.react(agree);
   await msg.react(disagree);
 
   const reactions = await msg.awaitReactions(reaction => reaction.emoji.name === agree || reaction.emoji.name === disagree, {time: args[0]});
-  msg.delete();
-
-  var NO_Count = reactions.get(disagree).count;
-  var YES_Count = reactions.get(agree);
-
-  if(YES_Count == undefined){
-    var YES_Count = 1;
-  }else{
-    var YES_Count = reactions.get(agree).count;
-  }
-
-  var sumsum = new Discord.RichEmbed()
-  
-            .addField("Голосование завершено:", "----------------------------------------\n" +
-                                          "Вопрос: " + message.content.split(" ").splice(2).join(" ") + "\n" +
-                                          "Кол-во голосов (Yes): " + `${YES_Count-1}\n` +
-                                          "Кол-во голосов (NO): " + `${NO_Count-1}\n` +
-                                          "----------------------------------------", true)
-
-            .setColor("0x#FF0000")
-  await message.channel.send({embed: sumsum});
-
 }
 
 module.exports.help = {
